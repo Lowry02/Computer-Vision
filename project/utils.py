08bdae0ceb07db80d17e03f899c101f47e6f2294
Lowry02 <lorenzocusin02@gmail.com>
2025-12-09 00:41:23 +0100

    Adding the image in Ex2 and adding Ex3

--- project/utils.py
+++ project/utils.py
@@ -175,3 +175,148 @@ def get_extrinsic(K:np.ndarray, H:np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     t = lam * K_inv @ H[:, 2]
     R = np.stack([r1,r2,r3]).transpose()
     return R, t
+
+def get_projection_matrix(K: np.ndarray, R: np.ndarray, t: np.ndarray) -> np.ndarray:
+    """
+    Computes the projection matrix for a camera given its intrinsic matrix, 
+    rotation matrix, and translation vector.
+    Args:
+        K (np.ndarray): The intrinsic matrix of the camera (3x3).
+        R (np.ndarray): The rotation matrix representing the camera's orientation (3x3).
+        t (np.ndarray): The translation vector representing the camera's position (3,).
+    Returns:
+        np.ndarray: The projection matrix (3x4) obtained by combining the intrinsic 
+        and extrinsic parameters.
+    """
+    
+    assert isinstance(K, np.ndarray), f"K is not a numpy array: type {type(K)}."
+    assert K.shape == (3, 3), f"K does not have shape (3, 3): shape {K.shape}."
+    assert isinstance(R, np.ndarray), f"R is not a numpy array: type {type(R)}."
+    assert R.shape == (3, 3), f"R does not have shape (3, 3): shape {R.shape}."
+    assert isinstance(t, np.ndarray), f"t is not a numpy array: type {type(t)}."
+    assert t.shape in [(3,), (3, 1)], f"t does not have shape (3,) or (3,1): shape {t.shape}."
+    
+    G = np.zeros((3, 4))
+    G[:, :3] = R
+    G[:, 3] = t
+    return  K @ G
+
+def project(points:np.ndarray, P:np.ndarray) -> np.ndarray:
+    if len(points.shape) == 1:
+        points = np.expand_dims(points, axis=0)
+    
+    projected_u = (P[0, :] @ points.transpose()) / (P[2, :] @ points.transpose())
+    projected_v = (P[1, :] @ points.transpose()) / (P[2, :] @ points.transpose())
+    
+    return np.stack([np.array([u, v]) for u, v in zip(projected_u, projected_v)])
+    
+def superimpose_cylinder(
+    img_path: str, 
+    P: np.ndarray,
+    radius: float, 
+    height: float, 
+    center_x: float, 
+    center_y: float, 
+    num_sides: int = 30, 
+    num_height_slices: int = 5
+) -> np.ndarray:
+    """
+    Generate the 3D points of a cylinder, calculate the projection matrix and 
+    overlay the object onto the 2D image.
+
+    Parameters:
+    -----------
+    img_path : str
+        The path to the image file.
+    P : np.ndarray
+        Projection matrix
+    radius : float
+        The radius of the cylinder (in the world reference frame).
+    height : float
+        The height of the cylinder (in the world reference frame).
+    centre_x : float
+        The X coordinate of the centre of the base (in the world reference frame).
+    centre_y : float
+        The Y coordinate of the centre of the base (in the world reference frame).
+    num_sides : int 
+        Number of sides to approximate the circle.
+    num_height_slices : int
+        Number of sections along the height.
+
+    Returns:
+    --------
+    np.ndarray
+        The image with the cylinder superimposed.
+    """
+    assert isinstance(img_path, str), f"img_path is not a string: type {type(img_path)}."
+    assert isinstance(P, np.ndarray), f"P is not a numpy array: type {type(P)}."
+    assert P.shape == (3, 4), f"P does not have shape (3, 4): shape {P.shape}."
+    assert isinstance(radius, (int, float)), f"radius is not a number: type {type(radius)}."
+    assert isinstance(height, (int, float)), f"height is not a number: type {type(height)}."
+    assert isinstance(center_x, (int, float)), f"center_x is not a number: type {type(center_x)}."
+    assert isinstance(center_y, (int, float)), f"center_y is not a number: type {type(center_y)}."
+    assert isinstance(num_sides, int), f"num_sides is not an integer: type {type(num_sides)}."
+    assert num_sides >= 3, f"num_sides is less than 3: value {num_sides}."  
+
+    
+    # Generation of 3D Points 
+    theta = np.linspace(0, 2 * np.pi, num_sides, endpoint=False)
+    z_slices = np.linspace(0, height, num_height_slices) 
+    points_3d = []
+
+    # Perimetral points for each height slice
+    for z in z_slices:
+        x = center_x + radius * np.cos(theta)
+        y = center_y + radius * np.sin(theta)
+        Z = np.full_like(x, z)
+        # Coordinate omogenee [x, y, z, 1]
+        homogeneous_slice = np.vstack((x, y, Z, np.ones_like(x))).T
+        points_3d.append(homogeneous_slice)
+
+    # Bottom slice (Z = 0)
+    x_base = center_x + radius * np.cos(theta)
+    y_base = center_y + radius * np.sin(theta)
+    base_points = np.stack([x_base, y_base, np.zeros_like(x_base), np.ones_like(x_base)], axis=1)
+    points_3d.append(base_points)
+
+    # Top slice (Z = height)
+    x_top = center_x + radius * np.cos(theta)
+    y_top = center_y + radius * np.sin(theta)
+    top_points = np.stack([x_top, y_top, np.full_like(x_top, -height), np.ones_like(x_top)], axis=1)    # TODO: Z axis is inverted
+    points_3d.append(top_points)
+    
+    object_points = np.concatenate(points_3d, axis=0) # shape (N, 4)
+    
+    # Pojection of 3D Points onto the Image Plane
+    img = cv2.imread(img_path)
+    if img is None:
+        raise FileExistsError(f"Cannot find the image file at {img_path}.")
+
+    points_2D_hom = project(object_points, P)
+    u = points_2D_hom[:, 0]
+    v = points_2D_hom[:, 1]
+    
+    projected_points = np.stack([u, v], axis=1).astype(np.int32) 
+
+    # Base
+    base_start_idx = projected_points.shape[0] - 2 * num_sides
+    base_points_proj = projected_points[base_start_idx : base_start_idx + num_sides]
+    
+    # Top
+    top_points_proj = projected_points[base_start_idx + num_sides : ]
+
+    # Drawing base
+    cv2.polylines(img, [base_points_proj], isClosed=True, color=(0, 0, 255), thickness=2) 
+    
+    # Drawing top
+    cv2.polylines(img, [top_points_proj], isClosed=True, color=(0, 255, 0), thickness=2) 
+    center_projection = project(np.array([center_x, center_y, 0, 1]), P)[0]
+    cv2.circle(img, (int(center_projection[0].round()), int(center_projection[1].round())), radius=5, color=(255, 0, 0), thickness=-1)
+    # Drawing vertical sides
+    side_idxs = np.linspace(0, num_sides - 1, num_sides, dtype=int)
+    for side in side_idxs:
+        pt_base = base_points_proj[side]
+        pt_top = top_points_proj[side]
+        cv2.line(img, tuple(pt_base), tuple(pt_top), color=(255, 0, 0), thickness=1)
+        
+    return img
